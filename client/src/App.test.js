e2edac1dcfa962a03c6ffc1fe28c4afb98204e89
Tristan Boudreau <tristanboudreau@Tristans-MacBook-Pro.local>
2019-11-01 11:41:44 -0600

    mvp rough, 2 intentially failed tests, 1 passing test, basic styling

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,7 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import * as rtl from '@testing-library/react';
 
+afterEach(rtl.cleanup);
+
+test('Render the app (this one is supposed to fail)', () => {
+  const wrapper = rtl.render(<App />)
+  const element = wrapper.getByText(/Men's World Cup/i)
+  expect(element).toBeVisible()
+})
+test('Render count input (should also fail)', () => {
+  const wrapper = rtl.render(<App />)
+  const element = wrapper.getByText(/Best US Players/i)
+  expect(element).toBeVisible()
+})
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
